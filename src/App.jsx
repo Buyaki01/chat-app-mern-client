292a87c87d25f2059d5410df3477a61add78b8c7
Buyaki01 <rittahbuyaki@gmail.com>
2024-01-31 09:26:55 +0300

    Install axios, add axios.defaults.baseURL, add registerUser method

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import axios from 'axios'
 import './App.css'
 import Register from './components/Register'
 
 function App() {
+  axios.defaults.baseURL = 'http://localhost:5000'
+  axios.defaults.withCredentials = true
 
   return (
     <>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,13 +1,19 @@
+import axios from 'axios'
 import { useState } from 'react'
 
 const Register = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
+  const registerUser = async (e) => {
+    e.preventDefault()
+    await axios.post('/register', { username, password })
+  }
+
   return (
     <div className='h-screen flex items-center justify-center'>
       <div className='shadow-2xl p-4'>
-        <form className='w-72 mx-auto'>
+        <form className='w-72 mx-auto' onSubmit={registerUser}>
           <input 
             type='text' 
             placeholder='username' 
